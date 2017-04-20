5ac633ee8f43408e87f141426724ce455a1e486b
Bittern Technology <bitterntech@gmail.com>
2017-04-20 21:57:10 +0530

    changes

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,8 @@
 class ListingsController < ApplicationController
-  def index
 
+
+  def index
+    @map_data = Listing.pluck(:city, :latitude, :longitude)
   end
 
   def show
@@ -19,8 +21,10 @@ class ListingsController < ApplicationController
   def create
     @listing = Listing.new(listing_params)
       if @listing.save
-        params[:listing]['pictures'].each do |file|
-          @listing.pictures.create!(:picture_json => file)
+        if params[:listing]['pictures'].present?
+          params[:listing]['pictures'].each do |file|
+            @listing.pictures.create!(:picture_json => file)
+          end
         end
           flash[:notice] = "Listing added successfully!"
           redirect_to '/apartments'
